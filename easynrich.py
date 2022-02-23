c6a1936d6cb4d7135d7e1bab21d8344813146321
Sh0ckFR <10033649+Sh0ckFR@users.noreply.github.com>
2022-02-23 15:18:39 +0100

    Update easynrich.py

--- easynrich.py
+++ easynrich.py
@@ -12,22 +12,45 @@ class Dns2nrich:
 	def __init__(self):
 		self.ip_list = []
 
-	def launch_subfinder(self, domain):
+	def launch_subfinder(self):
 		'''Start subfinder on the domain'''
-		subprocess.Popen(['subfinder', '-d', domain, '-o', 'subfinder.txt']).wait()
+		if sys.argv[1].endswith('.txt'):
+			with open(sys.argv[1], 'r') as file:
+				for line in file:
+					line = line.strip()
+					if len(line) > 0:
+						subprocess.Popen(['subfinder', '-d', line, '-o', f'subfinder-{line}.txt']).wait()
+		else:
+			subprocess.Popen(['subfinder', '-d', sys.argv[1], '-o', f'subfinder-{sys.argv[1]}.txt']).wait()
 
 	def get_ip_addresses(self):
 		'''Get the IP addresses from the subdomains'''
-		with open('subfinder.txt', 'r') as file:
-			for line in file:
-				line = line.strip()
-				if len(line) > 0:
-					try:
-						ip_address = socket.gethostbyname(line)
-						if ip_address not in self.ip_list:
-							self.ip_list.append(ip_address)
-					except socket.gaierror:
-						pass
+		if sys.argv[1].endswith('.txt'):
+			with open(sys.argv[1], 'r') as file:
+				for line in file:
+					line = line.strip()
+					if len(line) > 0:
+						with open(f'subfinder-{line}.txt', 'r') as subfile:
+							for line in subfile:
+								line = line.strip()
+								if len(line) > 0:
+									try:
+										ip_address = socket.gethostbyname(line)
+										if ip_address not in self.ip_list:
+											self.ip_list.append(ip_address)
+									except (socket.gaierror, UnicodeError):
+										pass
+		else:
+			with open(f'subfinder-{sys.argv[1]}.txt', 'r') as file:
+				for line in file:
+					line = line.strip()
+					if len(line) > 0:
+						try:
+							ip_address = socket.gethostbyname(line)
+							if ip_address not in self.ip_list:
+								self.ip_list.append(ip_address)
+						except (socket.gaierror, UnicodeError):
+							pass
 
 	def launch_nrich(self):
 		'''Start nrich on the IP addresses from the subdomains'''
@@ -39,7 +62,7 @@ class Dns2nrich:
 def main():
 	'''Main'''
 	dns2nrich = Dns2nrich()
-	dns2nrich.launch_subfinder(sys.argv[1])
+	dns2nrich.launch_subfinder()
 	dns2nrich.get_ip_addresses()
 	dns2nrich.launch_nrich()
 
